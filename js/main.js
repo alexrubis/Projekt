0eb22b037322da4c4506eeca285c7e3bb171e3ff
Maciej Zarzeczny <maciej@pc116.home>
2019-12-09 18:17:54 +0100

    Added timers for each question - 09.12.2019

--- js/main.js
+++ js/main.js
@@ -1,4 +1,87 @@
 $(document).ready(function() {
+    var startTime = new Date().getTime();
+    var endTime;    
+    var startTimeSaved = false;
+
+    var firebaseConfig = {
+        apiKey: "AIzaSyCcCWbVcFj_6rG2eqxCfMoZRnyF1NHhmm4",
+        authDomain: "test-app-572fa.firebaseapp.com",
+        databaseURL: "https://test-app-572fa.firebaseio.com",
+        projectId: "test-app-572fa",
+        storageBucket: "test-app-572fa.appspot.com",
+        messagingSenderId: "247352338453",
+        appId: "1:247352338453:web:633baa043b785538bc461c"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    var db = firebase.database();
+
+    // Save new user to database
+    function createNewUser(username) {
+        var date = new Date();
+        var dateTime = date.getFullYear()+'-'+(date.getMonth()+1)+'-'+date.getDay()+'/'+date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
+        db.ref('users/' + username).set({
+            created: dateTime
+        });
+        localStorage.setItem('username', username);
+    }
+
+    // Save time to database that user took to answer the question
+    function saveTime(which) {        
+        var username = localStorage.getItem('username');
+        var questionNumber = localStorage.getItem('questionNo')             
+        // Time from showing question to first click
+        if (which == 1 && !startTimeSaved) {   
+            var time = new Date().getTime() - startTime;         
+            db.ref('users/' + username + "/" + questionNumber + "/startTime").set({
+                value: time
+            });
+            startTimeSaved = true;
+        } 
+        // Time from last click to submitting question
+        else if (which == 2) {                        
+            var time = new Date().getTime() - endTime;
+            db.ref('users/' + username + "/" + questionNumber + "/endTime").set({
+                value: time
+            });
+        } 
+        // Time that user take to complete while question
+        else if (which == 3) {
+            var time = new Date().getTime() - startTime;
+            db.ref('users/' + username + "/" + questionNumber + "/totalTime").set({
+                value: time
+            });
+        }
+    }
+
+    $('#firstQuestion').click(function() {
+        saveTime(1);
+    });
+
+    $('#lastQuestion').focus(function() {
+        endTime = new Date().getTime();
+    });
+
+    function displayTimes() {
+        var username = localStorage.getItem('username');
+        return db.ref('users/' + username).once('value', (snapshot) => {
+            var questionsCount = snapshot.numChildren() - 1;
+            for(i=1; i<=questionsCount; i++) {
+                var totalTime = snapshot.val()[i].totalTime.value;
+                var startTime = snapshot.val()[i].startTime.value;
+                var endTime = snapshot.val()[i].endTime.value;
+                console.log('Pytanie numer ' + i);
+                console.log('Total time: ' + totalTime / 1000 + 's');     
+                console.log('Start time: ' + startTime / 1000 + 's');
+                console.log('End time: ' + endTime / 1000 + 's');           
+
+                window.location.replace("index.html");
+                localStorage.setItem("questionNo", "");
+                localStorage.setItem("askedQuestions", "");
+            }
+        });        
+    }    
+
     // Get current question number
     questionNo = localStorage.getItem("questionNo");
     $(".question-no").html("Zadanie " + questionNo + "/6");
@@ -11,13 +94,12 @@ $(document).ready(function() {
 
     // Submit user data
     $("#user-data").submit(function(e) {
-
-        var data = $(this).serializeArray();
+        var data = $(this).serializeArray();        
+        createNewUser(data[0].value);
 
         if(inputValidation(data)) {
             // Set first question number
-            localStorage.setItem("questionNo", 1);
-            
+            localStorage.setItem("questionNo", 1);            
             goToNextQuestion();
         }
 
@@ -25,15 +107,15 @@ $(document).ready(function() {
     });
 
     // Submit question data
-    $("#question-data").submit(function(e) {
+    $("#question-data").submit(function(e) {        
+        saveTime(2);
+        saveTime(3);
         var data = $(this).serializeArray();
 
         if(inputValidation(data)) {
             if(questionNo == "6") {
                 // Return to main page & clear localStorage items
-                window.location.replace("index.html");
-                localStorage.setItem("questionNo", "");
-                localStorage.setItem("askedQuestions", "");
+                displayTimes();                                
             } else {
                 // Increment question number
                 localStorage.setItem("questionNo", ++questionNo);
@@ -53,7 +135,7 @@ $(document).ready(function() {
                 rand = Math.floor((Math.random() * 2) + 1);
             } else {
                 break;
-            }
+            }   
         }
 
         window.location.replace("question" + rand + ".html");
@@ -110,5 +192,5 @@ $(document).ready(function() {
             }
         });
         return response;
-    }
+    }    
 });
