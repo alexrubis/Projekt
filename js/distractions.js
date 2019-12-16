70d2decfc3d6f7e43b64d5b151f0b54120a17196
Maciej Zarzeczny <maciek3609@gmail.com>
2019-12-16 20:08:31 +0100

    16.12.2019 - Added simple setup funcionality

--- js/distractions.js
+++ js/distractions.js
@@ -1,5 +1,37 @@
 $(document).ready(function() {
 
+    $('#check1').change(function() {
+        this.checked ? $('#check1-form').removeClass('hidden') : $('#check1-form').addClass('hidden');
+    });
+
+    $('#check2').change(function() {
+        this.checked ? $('#check2-form').removeClass('hidden') : $('#check2-form').addClass('hidden');
+    });
+    
+    $('#check3').change(function() {
+        this.checked ? $('#check3-form').removeClass('hidden') : $('#check3-form').addClass('hidden');
+    });
+
+    $('#check4').change(function() {
+        this.checked ? $('#check4-form').removeClass('hidden') : $('#check4-form').addClass('hidden');
+    });
+
+    $('#check5').change(function() {
+        this.checked ? $('#check5-form').removeClass('hidden') : $('#check5-form').addClass('hidden');
+    });
+
+    $('#check6').change(function() {
+        this.checked ? $('#check6-form').removeClass('hidden') : $('#check6-form').addClass('hidden');
+    });
+
+    $('#check7').change(function() {
+        this.checked ? $('#check7-form').removeClass('hidden') : $('#check7-form').addClass('hidden');
+    });
+
+    $('#check8').change(function() {
+        this.checked ? $('#check8-form').removeClass('hidden') : $('#check8-form').addClass('hidden');
+    });
+
     $('#setup-form').submit(function(e) {
 
         var topLeft = $('#check1').is(":checked");
@@ -10,7 +42,7 @@ $(document).ready(function() {
         var bottomCenter = $('#check6').is(":checked");
         var bottomLeft = $('#check7').is(":checked");
         var leftCenter = $('#check8').is(":checked");
-        
+
         var active = [topLeft, topCenter, topRight, rightCenter, bottomRight, bottomCenter, bottomLeft, leftCenter];
 
         localStorage.setItem('activeDistractions', active);
